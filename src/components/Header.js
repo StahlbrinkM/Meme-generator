58204df66edd805924175dbcb185af66d59c446b
StahlbrinkM <marcus_ow@hotmail.com>
2022-09-20 14:34:17 +0200

    Meme generator WIP

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import trollface from "../img/trollface.png"
+
+function Header() {
+    return (
+      <header className="header">
+          <img alt="trollface"
+              src={trollface} 
+              className="header--image" 
+              />
+          <h2 className="header--title">Meme Generator</h2>
+          <h4 className="header--project">React Course - Project 3</h4>
+      </header>
+  )
+}
+
+export default Header;
